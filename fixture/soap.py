837d2fb3146a7fcae2b29b13ab87f9d09f2bfc7f
Toldo <toldzio@gmail.com>
2018-01-03 15:14:24 +0100

    Created method for getting contacts from soap

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,5 +1,6 @@
 __author__ = 'wdolowicz'
 
+from model.project import Project
 from suds.client import Client
 from suds import WebFault
 
@@ -14,15 +15,15 @@ class SoapHelper:
         try:
             client.service.mc_login(username, password)
             return True
-        except:
+        except WebFault:
             return False
 
     def get_project_list_with_soap(self, username, password):
-        self.projects_list = []
         client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
-            for element in client.service.mc_projects_get_user_accessible(username, password):
-                id = element.id
-                name = element.name
-                description = element.description
-                self.projects_list.append(Project(id=id, name=name, description=description))
-            return self.projects_list
+        self.projects_list = []
+        for element in client.service.mc_projects_get_user_accessible(username, password):
+            id = element.id
+            name = element.name
+            description = element.description
+            self.projects_list.append(Project(id=id, name=name, description=description))
+        return self.projects_list
