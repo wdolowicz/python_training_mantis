7cbfe833935da1bc05a048a36e2a9bb364fd82ed
Toldo <toldzio@gmail.com>
2018-01-03 15:14:47 +0100

    Modified tests to use soap

--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -8,18 +8,18 @@ import string
 def test_del_project_from_mantis(app):
     app.session.login("Administrator", "root")
     assert app.session.is_logged_in_as("administrator")
-    if len(app.project.get_project_list()) == 0:
+    if len(app.soap.get_project_list_with_soap("Administrator", "root")) == 0:
         project = Project(name=random_string("name", 10), description=random_string("description", 30))
         app.project.create(project)
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_list_with_soap("Administrator", "root")
     project = random.choice(old_projects)
     app.project.delete_project_by_id(project.id)
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_project_list_with_soap("Administrator", "root")
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 5
+    symbols = string.ascii_letters + string.digits + " "*5
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
